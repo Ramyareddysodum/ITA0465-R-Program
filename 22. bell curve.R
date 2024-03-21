c37eeb4221a5bd1c0534cdc43b5b0e17b7ecc090
Ramyareddysodum <124545939+Ramyareddysodum@users.noreply.github.com>
2024-03-21 21:22:53 +0530

    Update 22. array

--- 22. bell curve.R
+++ 22. bell curve.R
@@ -1,3 +1,11 @@
-n = floor(rnorm(10000, 500, 100))
-t = table(n)
-barplot(t)
+# Create three example arrays
+array1 <- array(1:9, dim = c(3, 3))
+array2 <- array(10:18, dim = c(3, 3))
+array3 <- array(19:27, dim = c(3, 3))
+
+# Combine the arrays
+combined_array <- rbind(array1[1,], array2[1,], array3[1,])
+
+# Print the combined array
+print(combined_array)
+
